4aa52887eae562c77792afb8eea23d4e976bd387
ewantes <andrew.chabanov@gmail.com>
2015-10-30 13:54:40 +0200

    alpha
    asas

---
+++ js/main.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function() {
+ 
+  var owl = $("#owl-demo");
+ 
+  owl.owlCarousel({
+      items : 3, //10 items above 1000px browser width
+      pagination : false
+     
+  });
+ 
+  // Custom Navigation Events
+  $(".next").click(function(){
+    owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+    owl.trigger('owl.prev');
+  })
+  $(".play").click(function(){
+    owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+  })
+  $(".stop").click(function(){
+    owl.trigger('owl.stop');
+  })
+ 
+});
